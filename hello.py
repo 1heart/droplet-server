c46d36c66719cca7544ecd328ed9dca1c752f690
1heart <yixin1996@gmail.com>
2015-01-06 22:03:51 -0500

    lol1

--- hello.py
+++ hello.py
@@ -1,8 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+drops = {}
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/data', method=("POST",))
+def post_data():
+	if not request.json or not 'title' in request.json:
+		abort(400)
+	drops[request.json['location']] = request.json['string']
+	
+	return "ty 4 data"
+
+@app.route('/data', method=("GET",))
+def get_data():
+	return jsonify(drops)
