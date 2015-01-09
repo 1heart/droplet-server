97c417e0a3374040fcbe226c41ffc764ab37ffab
1heart <yixin1996@gmail.com>
2015-01-09 17:24:00 -0500

    added different values

--- hello.py
+++ hello.py
@@ -3,21 +3,30 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-drops = {'lol': 'lol'}
+drops = []
 
 @app.route('/')
 def hello():
     return 'Hello World!'
 
-@app.route('/data', methods=['POST'])
+@app.route('/post', methods=['POST'])
 def post_data():
-	title = request.json['title']
-	text = request.json['text']
-	drops[title] = text
+	drop = {}
+	try:
+		drop[title] = request.json['title']
+		drop[user] = request.json['user']
+		drop[text] = request.json['text']
+		drop[lat] = request.json['lat']
+		drop[longi] = request.json['longi']
+		drop[score] = request.json['score']
+	except:
+		return "not all values were correct"
+	drops.append(drop)
 	return "ty 4 data"
+
 @app.route('/get', methods=['GET'])
 def get_data():
 	return jsonify(drops)
 
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == '__main__':
+    app.run()
--- python-http.py
+++ python-http.py
@@ -1,13 +1,14 @@
 import http.client, json
 
 data = {
-        'title': 'lol'
+        'title': 'haha',
+        'text': 'rofl'
 }
 
 headers = {'Content-type': 'application/json'}
 
 
-h1 = http.client.HTTPConnection('127.0.0.1:5000')
+h1 = http.client.HTTPConnection("sheltered-chamber-7333.herokuapp.com")
 h1.request("POST", "data", json.dumps(data), headers)
 response = h1.getresponse()
 print(response.read().decode())
